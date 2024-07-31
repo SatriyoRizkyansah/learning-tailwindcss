d42dfe99f558fa2fe65bd5ef2bb431d615f2003d
satriyo <satttrioo@gmail.com>
2024-07-31 12:23:40 +0700

    function & directives

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      spacing: {},
+      fontFamily: {
+        inter: ["Inter"],
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
